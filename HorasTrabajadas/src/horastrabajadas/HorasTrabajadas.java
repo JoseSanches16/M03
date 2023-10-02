285a120aeec2d5b19b6364dbce0102b2d2d18b87
JoseSanches16 <sanchesjose801@gmail.com>
2023-10-02 14:59:26 +0200

    HorasTrabajadas

---
+++ HorasTrabajadas/src/horastrabajadas/HorasTrabajadas.java
@@ -0,0 +1,27 @@
+
+package horastrabajadas;
+import java.util.Scanner;
+public class HorasTrabajadas {
+
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+int horas , tarifasNormal , tarifasExtras ;
+
+
+        System.out.println("horas normales");
+       
+        tarifasNormal = 20;
+        tarifasExtras = 35;
+
+
+       if ()
+       {
+           System.out.println("Salario Base");
+       }
+
+
+
+
+    }   
+}
