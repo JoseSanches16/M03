71c9960b611df03ba21a145559d70aa7e797e3b0
JoseSanches16 <sanchesjose801@gmail.com>
2023-10-07 23:58:26 +0200

    sumando 1en1

---
+++ del1al200sumando1en1/src/del/pkg1/al/pkg200/sumando/pkg1en1/Del1Al200Sumando1en1.java
@@ -0,0 +1,29 @@
+package del.pkg1.al.pkg200.sumando.pkg1en1;
+public class Del1Al200Sumando1en1 {
+ public static void main(String[] args) {
+   
+
+
+int cont ;
+   
+   for (cont=1; cont <201; cont++)
+   
+   {
+            System.out.println(" contador  -->"  + cont);
+   }
+   
+   cont = 0;
+   do
+   {
+        
+        System.out.println("Has introducido el " + cont);           
+     cont++;
+  
+      } 
+    while (cont <=200);
+     System.out.println("final");
+
+
+ }
+    
+}
