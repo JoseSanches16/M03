f88cd0ca44f153895ff074b31ab6e72488ec8746
JoseSanches16 <sanchesjose801@gmail.com>
2023-10-08 15:12:51 +0200

    Ejercicios para practicar

---
+++ Ejercicio de practica1/src/ejercicio/de/practica1/EjercicioDePractica1.java
@@ -0,0 +1,14 @@
+package ejercicio.de.practica1;
+public class EjercicioDePractica1 {
+    public static void main(String[] args) {
+   
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
