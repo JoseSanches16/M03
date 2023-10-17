ba475caeefda81fdd06bda7ede439d73120b90df
JoseSanches16 <sanchesjose801@gmail.com>
2023-10-17 11:24:17 +0200

    Adivina el numero

--- AdivinaNumero/src/adivinanumero/AdivinaNumero.java
+++ AdivinaNumero/src/adivinanumero/AdivinaNumero.java
@@ -7,27 +7,51 @@ public class AdivinaNumero {
     
     Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        int numeroAleatorio = random.nextInt(101); 
-        int intentos = 0;
+     int num_usuari, intentos=0;
+        int max = 20;
+        int min = 1;
+        int range = max - min + 1;  
+        int numero_correcte = (int)(Math.random()*range) + min;
+        System.out.println("He pensat el " + numero_correcte);
 
-        System.out.println(" un numero entre 0 y 100. Adivina cual es.");
-
-        int suposicion;
-
-        do {
-            System.out.print("Ingresa tu suposicion: ");
-            suposicion = sc.nextInt();
+         do{
+           
+            System.out.println("Pon un valor del " + min + " al " + max);
+            num_usuari = sc.nextInt();
             intentos++;
-
-            if (suposicion < numeroAleatorio) {
-                System.out.println("El numero es mayor. Intenta de nuevo.");
-            } else if (suposicion > numeroAleatorio) {
-                System.out.println("El numero es menor. Intenta de nuevo.");
+            if (num_usuari>numero_correcte)
+            {
+                System.out.println("tu numero es mayor");
             }
-        } while (suposicion != numeroAleatorio);
+            else if (num_usuari<numero_correcte)
+            {
+                System.out.println("tu numero es menor");
+            }
+            
+            if (intentos>=3)
+            {
+            break;
+            }
+            
+//            else
+//            {
+//                System.out.println("Es correcto");
+//            }
+        }while(num_usuari!=numero_correcte  && intentos<3);
+   //negar las condiciones    (num_usuari = num_correcte  || intentos>=3);
+        
+   if (intentos>=3){
+    
+        System.out.println("has perdido");
+    }
 
-        System.out.println("Felicidades  Adivinaste el numero en " + intentos + " intentos.");
-      
+   
+   else {
+   
+      System.out.println("Es correcto has tardado " + intentos + " intentos .... ");
+   }
+    
+    }
     }
     
-}
+
